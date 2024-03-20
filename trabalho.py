6564987d8f9407e7681cb0cfa8bb18823f31ba9c
feopslima <159271486+feopslima@users.noreply.github.com>
2024-03-20 10:00:58 -0300

    trabalho aula 6

---
+++ trabalho.py
@@ -0,0 +1,36 @@
+print ("fernanda")
+
+numero = 10
+nome = "Jõao"
+booleano = True
+
+
+numero = 1
+numero2 = 2
+numero3 = 11
+numero4 = 91
+frase = "o outono inicia hoje"
+palavra = "outono"
+
+media = numero + numero2 + numero3 +numero4 / 4
+print ("A média é", media)
+
+quadrado = numero**2
+print ("o numero ao quadrado é", quadrado)
+
+dobro = numero2*2
+print ("o dobro do numero2 é", dobro)
+
+palavra = "outono"
+tamanho = len(palavra)
+print ("a quantidade de letras da palavra é", tamanho)
+
+frase = "o outono inicia hoje"
+quantidade = frase.count(' ')
+print ("a quantidade de espaços em branco da frase é", quantidade)
+
+maior = max (numero, numero2, numero3, numero4)
+print ("o maior número é", maior)
+
+maiornumero = numero > numero3
+print ("o primeiro é maior que o segundo?", maiornumero)
